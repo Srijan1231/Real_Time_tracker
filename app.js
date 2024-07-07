17039752967d53be915a6225347c58d1aa9b5cc8
srijan <srijan.dhl21@gmail.com>
2024-07-08 01:18:54 +1000

    final_of_RTT

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  socket.on("send-locations", (data) => {
+    io.emit("received-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnect", () => {
+    io.emit("user-disconnected", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+});
